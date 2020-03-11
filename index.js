c26e59d5bf64306500cd55bef8253a760c88b277
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2020-03-11 17:41:04 +0200

    did stuff

--- index.js
+++ index.js
@@ -1,43 +1,58 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-  var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'line',
+const finns = 5528442; // Population, by end of Jan 2020.
+let totalInfected = infections[infections.length-1];
 
-    // The data for our dataset
-    data: {
-      labels: dates,
-      datasets: [{
-        label: 'Tartuntoja yhteensä',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: infections,
-        lineTension: 0,
-        pointRadius: 1
-      },
-      {
-        label: 'Tartuntoja per päivä',
-        backgroundColor: 'rgba(175, 155, 35, 0.5)',
-        borderColor: 'rgb(175, 155, 35)',
-        data: infectionsPerDay,
-        lineTension: 0,
-        pointRadius: 1
-      },
-      {
-        label: 'Koronakuolemia yhteensä',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        borderColor: 'rgb(0, 0, 0)',
-        data: deaths,
-        lineTension: 0,
-        pointRadius: 1
-      }]
+document.getElementById('totalNumber').textContent = totalInfected;
+document.getElementById('infectedPercent').textContent = (totalInfected / finns * 100).toFixed(5) + "%";
+
+
+let ctx1 = document.getElementById('koronaKuvaaja').getContext('2d');
+let chart1 = new Chart(ctx1, {
+  // The type of chart we want to create
+  type: 'line',
+
+  // The data for our dataset
+  data: {
+    labels: dates,
+    datasets: [{
+      label: 'Tartuntoja yhteensä',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: infections,
+      lineTension: 0,
+      pointRadius: 1
     },
+    {
+      label: 'Tartuntoja per päivä',
+      backgroundColor: 'rgba(175, 155, 35, 0.5)',
+      borderColor: 'rgb(175, 155, 35)',
+      data: infectionsPerDay,
+      lineTension: 0,
+      pointRadius: 1
+    },
+    {
+      label: 'Koronakuolemia yhteensä',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      borderColor: 'rgb(0, 0, 0)',
+      data: deaths,
+      lineTension: 0,
+      pointRadius: 1
+    }]
+  },
 
-    // Configuration options go here
-    options: {
-      tooltips: {
-        mode: "index",
-        intersect: false,
-        position: "nearest"
-      }
+  // Configuration options go here
+  options: {
+    tooltips: {
+      mode: "index",
+      intersect: false,
+      position: "nearest"
+    },
+    scales: {
+      yAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'hlö'
+        }
+      }]
     }
-  });
+  }
+});
