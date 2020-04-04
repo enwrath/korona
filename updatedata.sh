9c15e7b1de9638d4ef47341a693c8e5d167fe9c6
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2020-04-04 16:13:58 +0300

    get v2 data

--- updatedata.sh
+++ updatedata.sh
@@ -1,7 +1,7 @@
 # Lataa sairasdata ja heitä gittiin
 # Eeeeeehkä voisi tehdä järkevämminkin mutta eeeeeeh.
 git pull
-curl https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData -o data/data.json
+curl https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData/v2 -o data/data.json
 git add data/data.json
 git commit -m "Automatic data update"
 git push origin master
